323e505e5b6349d4fc83d1c4bbe979456f0b6ac3
scottsievert <sievert.scott@gmail.com>
2013-06-07 12:37:25 -0500

    Revert "Revert "…writeBMP failed.""
    This reverts commit 4dd114ec607de931c6d44ef9b56e8ad8cfeb92a1.

--- IRcamera.ino
+++ IRcamera.ino
@@ -31,16 +31,17 @@
  */
 
 #include <SD.h>
-//#include <SPI.h>
+#include <SPI.h>
 //#include <Wire.h>
 //#include <EEPROM.h>
 #include <i2cmaster.h>
 //#include <I2C.h>
-//#include <SDFat.h>
-//#include <SDFatUtil.h>
+//#include <SdFat.h>
+//#include <SdFatUtil.h>
 const int chipSelect = 8;
 // must change from 4 to 8! (8 on sparkfun shield)
-
+//SdFat sd;
+//SdFile file;
 //SdFat sd;
 //SdFile file;
 const uint8_t cardPin = 8;                    
@@ -55,10 +56,10 @@ void setup(){
     //Serial.println("At beginning of setup...\n");
     pinMode(10, OUTPUT); pinMode(9, OUTPUT);
     //digitalWrite(10, HIGH);
-    if (!SD.begin(chipSelect)){
-        Serial.println("It did the SD-if statement");
-        return;
-    }
+//    if (!SD.begin(chipSelect)){
+//        Serial.println("It did the SD-if statement");
+//        return;
+//    }
     Serial.println("Done with serial port setup...\n");
 
 
@@ -93,12 +94,12 @@ void setup(){
     
     int width  = 9;
     int height = 9;
-    
+    Serial.println("In setup(), before the malloc calls");
     float * x = takePicture(width, height);
     int * xx = (int *)malloc(sizeof(int)*width*height);
     
     for (int i=0; i<width*height; i++){
-        xx[i] = (int)(x[i] * 10);
+        xx[i] = (int)(x[i] * 5);
     }
     for(int i=0; i<width*height; i++){
       Serial.print(i); Serial.print("     ");
@@ -106,17 +107,37 @@ void setup(){
       Serial.print(x[i]); Serial.println("   in setup");
     }
     writeBMPImage(xx,"debug.bmp" ,width, height);
+    
+    
+    // what you need to do before writing will work! THIS WORKS
+//    SdFile myFile;
+//    if (!sd.begin(cardPin, SPI_QUARTER_SPEED)) {
+//        sd.errorHalt("sd.begin(...) failed.");
+//        Serial.println("if-SD card Error!!!");
+//        Serial.println("---");
+//    }
+//    if (!myFile.open("test.txt", O_RDWR | O_CREAT | O_AT_END)) {
+//        sd.errorHalt("opening test.txt for write failed");
+//    }
+//    myFile.write("Writing, uh-huh");
+//    myFile.print("Will it work? Please do.");
+//    Serial.println("Done writing to myFile");
+//    myFile.close();
+
 }
 
 void loop(){
   float temp = 0;
   temp = readTemp();
   Serial.print(temp); Serial.println(" degrees Celcius");
+  delay(1000);
 }
 
-float* takePicture(int width, int height){
+float * takePicture(int width, int height){
     // assumes width, height of 11, 11
     // 
+    Serial.println("In takePicture(...)");
+    Serial.println("\n\n\n");
     int xx, yy;
 
     int HORIZPIN = 9;
@@ -129,7 +150,7 @@ float* takePicture(int width, int height){
             gotoPixel(xx, yy, HORIZPIN, VERTPIN, width, height);
             x[yy*width + xx] = readTemp();
             Serial.print("   "); Serial.println(x[yy*width + xx]);
-            delay(100);
+            delay(10);
         }      
     }
     
@@ -186,6 +207,7 @@ void gotoPixel(int vertical, int horizontal, int vertPin, int horizPin, int heig
 
 
 
+
 void writeBMPImage(int * input, char fileName[], int w, int h){
     //Serial.write("width == "); Serial.println(w);
     //Serial.write("height == "); Serial.println(h);
@@ -194,14 +216,14 @@ void writeBMPImage(int * input, char fileName[], int w, int h){
     // "toDebug.bmp"
     
     //Serial.begin(9600);
-    //if (!sd.init(SPI_FULL_SPEED, cardPin)) {
-    //    sd.initErrorHalt();
-    //    Serial.println("---");
-    //}
+//    if (!sd.init(SPI_FULL_SPEED, cardPin)) {
+//        sd.initErrorHalt();
+//        Serial.println("---");
+//    }
     char name[] = "img4.bmp";
-    Serial.print("In the writeBMPImage, writing ");
-    Serial.write(name);
-    Serial.write("\n\n");
+//    Serial.print("In the writeBMPImage, writing ");
+//    Serial.write(name);
+//    Serial.write("\n\n");
    
     
 
@@ -314,3 +336,104 @@ void writePlainTextImage(int * pix, int w, int h){
     
   
 }
+
+
+
+
+
+// ---- below function didn't work. reverting to writeBMPImage()
+//void writeBMPImage2(int * input, char filename[], int w, int h){
+//    Serial.println("In writeBMPImage2 (TWO, OKAY)");
+//    int debugPrint = 1;
+//    char name[] = "bug2.bmp";
+//    if (!sd.begin(cardPin, SPI_QUARTER_SPEED)) {
+//        sd.errorHalt("In writeBMPImage2: sd.begin(...) failed");
+//        Serial.println("if-SD card Error!!!");
+//        Serial.println("---");
+//    }
+//    //Serial.print("errorCode() == ");
+//    //Serial.println(sd.errorCode());
+//    if (!file.open(name, O_RDWR | O_CREAT | O_AT_END)) {
+//        sd.errorHalt("opening <name>.bmp for write failed");
+//    }
+//
+//     // set fileSize (used in bmp header)
+//  int rowSize = 4 * ((3*w + 3)/4);      // how many bytes in the row (used to create padding)
+//  int fileSize = 54 + h*rowSize;        // headers (54 bytes) + pixel data
+//
+//  // create image data; heavily modified version via:
+//  // http://stackoverflow.com/a/2654860
+//  unsigned char *img = NULL;            // image data
+//  if (img) {                            // if there's already data in the array, clear it
+//    free(img);
+//  }
+//  int imgSize = w*h;
+//  int y, x;
+//  int px[w*h];
+//  img = (unsigned char *)malloc(3*imgSize);
+// 
+//
+//  for (int y=0; y<h; y++) {
+//    for (int x=0; x<w; x++) {
+//      int colorVal = px[y*w + x];                        // classic formula for px listed in line
+//      img[(y*w + x)*3+0] = (unsigned char)(colorVal);    // R
+//      img[(y*w + x)*3+1] = (unsigned char)(colorVal);    // G
+//      img[(y*w + x)*3+2] = (unsigned char)(colorVal);    // B
+//      // padding (the 4th byte) will be added later as needed...
+//    }
+//  }
+////    // print px and img data for debugging
+////  if (debugPrint) {
+////    Serial.print("\nWriting \"");
+////    Serial.print(name);
+////    Serial.print("\" to file...\n");
+////    //Serial.print("file == "); Serial.println(file, HEX);
+//////    for (int i=0; i<imgSize; i++) {
+//////      //Serial.print(px[i]);
+//////      //Serial.print("  ");
+//////    }
+////  }
+//
+//  // create padding (based on the number of pixels in a row
+//  unsigned char bmpPad[rowSize - 3*w];
+//  for (int i=0; i<sizeof(bmpPad); i++) {         // fill with 0s
+//    bmpPad[i] = 0;
+//  }
+//
+//  // create file headers (also taken from StackOverflow example)
+//  unsigned char bmpFileHeader[14] = {            // file header (always starts with BM!)
+//    'B','M', 0,0,0,0, 0,0, 0,0, 54,0,0,0   };
+//  unsigned char bmpInfoHeader[40] = {            // info about the file (size, etc)
+//    40,0,0,0, 0,0,0,0, 0,0,0,0, 1,0, 24,0   };
+//    
+////  Serial.println("Before the BMP stuff");
+//  bmpFileHeader[ 2] = (unsigned char)(fileSize      );
+//  bmpFileHeader[ 3] = (unsigned char)(fileSize >>  8);
+//  bmpFileHeader[ 4] = (unsigned char)(fileSize >> 16);
+//  bmpFileHeader[ 5] = (unsigned char)(fileSize >> 24);
+//
+//  bmpInfoHeader[ 4] = (unsigned char)(       w      );
+//  bmpInfoHeader[ 5] = (unsigned char)(       w >>  8);
+//  bmpInfoHeader[ 6] = (unsigned char)(       w >> 16);
+//  bmpInfoHeader[ 7] = (unsigned char)(       w >> 24);
+//  bmpInfoHeader[ 8] = (unsigned char)(       h      );
+//  bmpInfoHeader[ 9] = (unsigned char)(       h >>  8);
+//  bmpInfoHeader[10] = (unsigned char)(       h >> 16);
+//  bmpInfoHeader[11] = (unsigned char)(       h >> 24);
+//
+//  // write the file (thanks forum!)
+//  file.write(bmpFileHeader, sizeof(bmpFileHeader));    // write file header
+//  file.write(bmpInfoHeader, sizeof(bmpInfoHeader));    // " info header
+//
+////  Serial.println("Before the for loop where it actually writes");
+//  for (int i=0; i<h; i++) {                            // iterate image array
+//    file.write(img+(w*(h-i-1)*3), 3*w);                // write px data
+//    file.write(bmpPad, (4-(w*3)%4)%4);                 // and padding as needed
+//  }
+//  file.close();                                        // close file when done writing
+//
+//  if (debugPrint) {
+//    Serial.print("\n\n---\n");
+//  }
+//
+//}

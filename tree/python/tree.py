a54a4d428d5282c8826edb864eddd5e55b558518
scottsievert <sieve121@umn.edu>
2013-12-23 17:33:15 -0600

    1D -- done.

--- tree/python/tree.py
+++ tree/python/tree.py
@@ -29,36 +29,10 @@ def sampleInDetail(interestedIn, sampleAt, h=None):
     # collect the unique terms
     sampleAt = unique(sampleAt)
     return sampleAt
-def approxWavelet(x, sampleAt, interestedIn, h=None):
-    """
-        approximates the wavelet transform but doesn't scale the wavelet
-    """
-    x_hat = x[sampleAt]
-    h_hat = h[: , sampleAt ]
-    h_hat = h_hat[interestedIn, :]
-    w_hat = h_hat.dot(x_hat)
-    # now we have the proper wavwelet, but not scaled
-
-    w_hat2 = zeros(len(x))
-    w_hat2[interestedIn] = w_hat
-    return w_hat2
 def findIndiciesThatMatter(element):
     i = argwhere(h[element,:] != 0)
     i = reshape(i, -1)
     return i
-def scaleWavelet(w_hat):
-    """
-        n2 for n^2, the number of terms  you sample at
-        m  for the number of samples in the space
-    """
-    for i in arange(len(w_hat)):
-        if w_hat[i] == 0: continue
-        n2 = findIndiciesThatMatter(i)
-        m = argwhere(n2 == sampleAt[:,newaxis])
-        m = len(m)
-        n2 = len(n2)
-        w_hat[i] = w_hat[i] * n2 / m
-    return w_hat
 def addToInterestedIn(interestedIn, level):
     """ level=XXX for the base term. it's the level we're going to """
     # we have [0 1 2 3] ==> [0 1 2 3 4 5 6 7]
@@ -72,8 +46,12 @@ def addToInterestedIn(interestedIn, level):
     interestedIn = unique(interestedIn)
 
     return interestedIn
-def scaleAndApproxWavelet(x, sampleAt, interestedIn, h=None):
-    """ """
+def scaleAndApproxWavelet(x, sampleAt, interestedIn, h=None, N=None):
+    """ 
+        deletes the rows and columns out of `h` then multiplies each term by
+        how many it terms it stands for -- how many empty spaces can be filled
+        by that one term.
+    """
     i = arange(len(sampleAt)-1, dtype=int)
     prod = sampleAt[i+1] - sampleAt[i]
     prod = hstack((prod, N-sampleAt[-1]))
@@ -87,59 +65,84 @@ def scaleAndApproxWavelet(x, sampleAt, interestedIn, h=None):
     ret[interestedIn] = w_hat
     return ret
 
-N = 2048
-h = haarMatrix(N)
-x = linspace(-1,1,N)
-a = 1/5
-x = exp(-x**2 / a**2) / (a * sqrt(pi))
-#x = ones(N)
+def oneD():
+    """
+        this function approximates the wavelet transform with edge detection <more description>.
 
-interestedIn = arange(6)
-sampleAt = array([], dtype=int)
+        we have to add to interestedIn (which wavelet terms we're interested in) each time;
+        we have to go down those branches. 
 
-sampleAt = sampleInDetail(interestedIn, sampleAt, h   = h)
-w_hat = scaleAndApproxWavelet(x, sampleAt, interestedIn, h=h)
+        for approximating the wavelet transform, we "fill in" for the missing pixels, 
+        kind of like a really poor inpainting job. what if for some wavelet term 
+        it's more clustered? we can't do `n / m` then.
+    """
+    N = 2**11
+    h = haarMatrix(N)
+    x = linspace(-1,1,N)
+    a = 1/5
+    x = exp(-x**2 / a**2) / (a * sqrt(pi))
+    #x = ones(N)
 
-# now, we can approximate the wavelet from an arbitrary number of samples. not
-# we need to choose the best places to sample at the right locations ==>
-    # 1. approximate the wavelet
-    # 2. see where non-zero
-    # 3. add the non-zero locations to "interestedIn"
+    interestedIn = arange(6)
+    sampleAt = array([], dtype=int)
 
-threshold = 0.5
-# in that arange(1,4), 0*2 = 0 ==> steady state, no change
-for iteration in arange(1,6): 
-    # adding to interestedIn
-    interestedIn = addToInterestedIn(interestedIn, iteration)
+    sampleAt = sampleInDetail(interestedIn, sampleAt, h   = h)
+    w_hat = scaleAndApproxWavelet(x, sampleAt, interestedIn, h=h, N=N)
 
-    # sample in detail
-    sampleAt = sampleInDetail(interestedIn, sampleAt, h=h)
+    # now, we can approximate the wavelet from an arbitrary number of samples. not
+    # we need to choose the best places to sample at the right locations ==>
+        # 1. approximate the wavelet
+        # 2. see where non-zero
+        # 3. add the non-zero locations to "interestedIn"
 
-    # approx and scale the wavelet
-    w_hat = scaleAndApproxWavelet(x, sampleAt, interestedIn, h=h)
+    # what's the threshold, and how far?
+    threshold = 0.5
+    level = 6
+    # in that arange(1,4), 0*2 = 0 ==> steady state, no change
+    for iteration in arange(1,level): 
+        threshold = 2**-iteration * 0.5
+        print "Threshold: ", threshold
 
-    # where is it greater than the threshold?
-    i = argwhere(abs(w_hat[:2**(iteration+1)]) >= threshold)
-    interestedIn = unique(i)
-    # we need to add to interestedIn. in the terms that are non-zero, we need to add
-    # the sub-terms, then sample in detail, then approx the wavelet
-    
-    # right. I went over this before. they might be more clustered by the negative terms.
+        # adding the subterms to interestedIn
+        interestedIn = addToInterestedIn(interestedIn, iteration)
 
+        # sample in detail
+        sampleAt = sampleInDetail(interestedIn, sampleAt, h=h)
 
-w = h.dot(x)
-figure()
-plot(w, marker='.', label='Exact')
-plot(w_hat, marker='.', label='Approx')
-legend()
-show()
+        # approx and scale the wavelet
+        w_hat = scaleAndApproxWavelet(x, sampleAt, interestedIn, h=h, N=N)
 
-figure()
-plot(inv(h).dot(w), marker='.')
-plot(inv(h).dot(w_hat), marker='.')
-show()
+        # where is it greater than the threshold?
+        # we're seeing where in the current "level" it's greater than the threshold
+        i = argwhere(abs(w_hat[:2**(iteration+1)]) >= threshold)
+        interestedIn = unique(i)
+        
+        # right. I went over this before. they might be more clustered by the negative terms.
 
 
+    w = h.dot(x)
+    figure()
+    plot(w[0:2**level], marker='.', label='Exact')
+    plot(w_hat[0:2**level], marker='.', label='Approx')
+    legend()
+    show()
 
-# 0 1 | 2 3 | 4 5 6 7 | 8 9 10 11 12 13 14 15
+    figure()
+    plot(sampleAt, 'o')
+    show()
+
+    exact = inv(h).dot(w)
+    approx = inv(h).dot(w_hat)
+
+    figure()
+    plot(inv(h).dot(w), marker=' ', label='Exact')
+    plot(inv(h).dot(w_hat), marker=' ', label='Approx')
+    plot(abs(exact - approx), label='Error')
+    legend()
+    show()
+
+    print "Sampling rate: ", len(sampleAt) / N
+
+
+    # 0 1 | 2 3 | 4 5 6 7 | 8 9 10 11 12 13 14 15
 
